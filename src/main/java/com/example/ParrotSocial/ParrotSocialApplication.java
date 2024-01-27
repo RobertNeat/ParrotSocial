e49e8beade485b6f568a009bf2bbe4e529567583
Robert Sereda <robertgoogl@gmail.com>
2024-01-27 12:27:07 +0100

    first commit

---
+++ src/main/java/com/example/ParrotSocial/ParrotSocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParrotSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrotSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrotSocialApplication.class, args);
+	}
+
+}
